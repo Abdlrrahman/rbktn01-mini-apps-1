c7103cf4dcb3b6eeaf5545326fd423280d94195d
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-12-27 20:04:07 +0100

    Almost done stll missing the tie

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,18 @@
 var counter = 0;
 var checker = {};
 var xValues = {};
-var xCounter = 0;
-var win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+var result;
+var check =[];
+
+var win = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
 function change(id) {
-	if (!checker[id]) {
+	check.push(id);
+	if (!result) {
+		if (!checker[id]) {
 		checker[id] = id;
 		if(counter % 2 === 0) {
 			counter++;
-			xValues[xCounter++] = id.slice(7);
-			console.log(xValues)
+			xValues[id.slice(7)] = 'X';
 			document.getElementById(id).innerText = "X";
 		}
 		else {
@@ -17,9 +20,20 @@ function change(id) {
 			document.getElementById(id).innerText = "O";
 		}
 	}
-	if (xCounter >= 3) {
-		if (xCounter % 2 === 1) {
-			document.getElementById("result").innerText = "YOU WON";
+			if (xValues[1] && xValues[2] && xValues[3] || xValues[4] && xValues[5] && xValues[6] || xValues[7] && xValues[8] && xValues[9] || xValues[1] && xValues[4] && xValues[7] || xValues[2] && xValues[5] && xValues[8] || xValues[3] && xValues[6] && xValues[9] || xValues[1] && xValues[5] && xValues[9] || xValues[3] && xValues[5] && xValues[7]) {
+				result = document.getElementById("result").innerText = "YOU WON";
+			}
+		else if (Object.values(xValues).length === 9) {
+			result = document.getElementById("result").innerText = "TIE";
 		}
-	}
+}}
+
+function rest() {
+	for (var i = 0; i < check.length; i++) {
+document.getElementById(check[i]).innerText = "";
+}
+counter = 0;
+checker = {};
+xValues = {};
+result = null;
 }
