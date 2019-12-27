a66ee79414c984c2b4d5b786c73540b71b3f07d6
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-12-27 20:07:57 +0100

    Done

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,8 +3,6 @@ var checker = {};
 var xValues = {};
 var result;
 var check =[];
-
-var win = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
 function change(id) {
 	check.push(id);
 	if (!result) {
@@ -23,7 +21,7 @@ function change(id) {
 			if (xValues[1] && xValues[2] && xValues[3] || xValues[4] && xValues[5] && xValues[6] || xValues[7] && xValues[8] && xValues[9] || xValues[1] && xValues[4] && xValues[7] || xValues[2] && xValues[5] && xValues[8] || xValues[3] && xValues[6] && xValues[9] || xValues[1] && xValues[5] && xValues[9] || xValues[3] && xValues[5] && xValues[7]) {
 				result = document.getElementById("result").innerText = "YOU WON";
 			}
-		else if (Object.values(xValues).length === 9) {
+		else if (Object.values(xValues).length === 5) {
 			result = document.getElementById("result").innerText = "TIE";
 		}
 }}
