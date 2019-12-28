342cab37cfb1ded48e0c671dd494085f5c413cdf
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-12-28 17:41:31 +0100

    done with the server printing hello world

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,7 +4,9 @@ const port = 3000
 
 app.use(express.static('client'))
 
-app.get('/index.js', (req, res) => {
-  res.send('Hello World!')})
+app.post('/something', express.json(), (req, res) => {
+  console.log(req.body)
+  res.send('Hello World!')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
