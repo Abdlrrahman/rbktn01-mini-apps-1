d8ae599bf3585731a55465e42f04a62583cefa99
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-12-30 19:28:37 +0100

    Done with the forms

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,87 @@
+class Form1 extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isHidden: true
+        }
+    }
+    toggleHidden() {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+    render() {
+        return (
+            <form onclick="event.preventDefault()" id="F1" method="POST" name="input1" action="/F2">
+                First name: <input type="text" name="fname"></input><br></br>
+                Last name: <input type="text" name="lname"></input><br></br>
+                Email: <input type="text" name="lname"></input><br></br>
+                <input type="password" name="psw"></input><br></br>
+                <input id="s1" onClick={this.toggleHidden.bind(this)} type="submit" value="Submit"></input>
+                {!this.state.isHidden && <Form2 />}
+            </form>
+        )
+    }
+}
 
+class Form2 extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isHidden: true
+        }
+    }
+    toggleHidden() {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+    render() {
+        return (
+            <form id="F2" method="POST" name="input2" action="/F3">
+                Line1: <input type="text" name="fname"></input><br></br>
+                Line2: <input type="text" name="lname"></input><br></br>
+                City: <input type="text" name="lname"></input><br></br>
+                State: <input type="text" name="lname"></input><br></br>
+                Zip Code: <input type="text" name="lname"></input><br></br>
+                Phone Number: <input type="number" name="lname"></input><br></br>
+                <input id="s2" onClick={this.toggleHidden.bind(this)} type="submit" value="Submit"></input>
+                {!this.state.isHidden && <Form3 />}
+            </form>
+        )
+    }
+}
 
+class Form3 extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isHidden: true
+        }
+    }
+    toggleHidden() {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+    render() {
+        return (
+            <form id="F3" method="POST" name="input2" action="/F3">
+                Credit Card Number: <input type="number" name="fname"></input><br></br>
+                Expiry Date: <input type="date" name="lname"></input><br></br>
+                CVV: <input type="text" name="lname"></input><br></br>
+                Billing Zip Code: <input type="text" name="lname"></input><br></br>
+                <input id="s3" type="submit" value="Submit"></input>
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(
+    <div><Form1 /></div>,
+    document.getElementById('title')
+);
+
+$("#F1").click(function (event) {
+    event.preventDefault();
+});
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000
+app.use(express.static('public'));
 
+app.post('/F2', (req, res) => res.send('Form 2'))
+app.post('/F3', (req, res) => res.send('Form 3'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
