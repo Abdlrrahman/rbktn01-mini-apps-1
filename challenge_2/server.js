8fb64458209e25c5681ecd5150e5566a28b53665
Abdlrrahman Shibani <56532938+Abdlrrahman@users.noreply.github.com>
2020-01-01 13:22:07 +0200

    Done

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+var jsonParser = bodyParser.json();
+app.use(express.static('client'));
+app.use(jsonParser);
+var fs = require('fs');
 
-app.use(express.static('client'))
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post('/something', express.json(), (req, res) => {
-  console.log(req.body)
-  res.send('Hello World!')
+app.post('/something', urlencodedParser, (req, res) => {
+  var parsed = JSON.parse(req.body.input);
+  var anything = children(parsed);
+  fs.writeFile('result.csv', anything, (error, data) => {
+    if (error) {
+      console.log(error);
+    }
+  })
+  res.send(anything);
 })
+function children(data) {
+  var array = [];
+
+  function iter(data) {
+    var obj = {};
+    for (var key in data) {
+      if (key !== 'children') {
+        obj[key] = data[key];
+      }
+    }
+    array.push(obj);
+    for (var i = 0; i < data['children'].length; i++) {
+      iter(data['children'][i]);
+    }
+  }
+  iter(data)
+  var names = [];
+  var values = [];
+  var valuesWithNewLine = [];
+
+  for (var i = 0; i < array.length; i++) {
+    names.push(Object.keys(array[i]));
+    values.push(Object.values(array[i]) + '\n');
+  }
+
+  return names[0] + '\n' + values;
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
